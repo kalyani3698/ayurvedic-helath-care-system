ab2c64f8a5888b01af84c568e2e08f989a9f7da0
kalyani3698 <dhavalakalyani34@gmail.com>
2026-04-13 12:43:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,9 @@ def create_app():
         db.create_all()
 
     return app
+app = create_app()   # <-- global app object
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, port=5000)
+
+
